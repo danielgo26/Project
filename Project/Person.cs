e17b8af7a405549add0a880496d53867b925acd8
Daniel Jordanov <danielgo_nz@abv.bg>
2021-02-27 14:32:18 +0200

    addindClassPerson

---
+++ Project/Person.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Project
+{
+    class Person
+    {
+        private int age;
+        private string name;
+        public int Age
+        {
+            get { return age; }
+            set { age = value; }
+        }
+       
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public void IndroduceYourself()
+        {
+            Console.WriteLine($"I am {this.Name}, {this.Age} years old.");
+        }
+
+
+    }
+}
